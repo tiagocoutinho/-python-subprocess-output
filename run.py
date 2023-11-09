05d211d277edeb1d1d3552089c5316025fb0646c
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2023-11-09 15:11:21 +0100

    Add process dots during sleep

--- run.py
+++ run.py
@@ -6,10 +6,16 @@ n, nap = sys.argv[1:]
 n = int(n)
 nap = float(nap)
 
+def sleep(nap):
+    x = int(nap * 10)
+    for i in range(x):
+        time.sleep(0.1)
+        print(".", end="")
+    print()
 
 print("start!")
 for i in range(n):
-    print(f"running {i+1:4}/{n} - sleep {nap} ")
-    time.sleep(nap)
+    print(f"running {i+1:4}/{n} - sleep {nap} ", end="")
+    sleep(nap)
 
 print("done!")
