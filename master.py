f9ca6ddc190dde46f19331ad465c6488c90f2501
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2023-11-09 20:58:35 +0100

    Handle event by event (not just newline)

--- master.py
+++ master.py
@@ -1,13 +1,21 @@
-
+import os
+import select
 import subprocess
 
 env = {
   "PYTHONUNBUFFERED": "1"
 }
-proc = subprocess.Popen(["python", "run.py", "10", "1"], stdout=subprocess.PIPE, env=env)
+proc = subprocess.Popen(["python", "run.py", "10", "1"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0, env=env)
 
-for line in proc.stdout:
-    line = line.decode()
-    print(line, end='', flush=True)
+os.set_blocking(proc.stdout.fileno(), False)
+
+reads = (proc.stdout,)
+while proc.poll() is None:
+    r, _, _ = select.select(reads, (), ())
+    if not r:
+        print("STOPPING!")
+        break
+    data = proc.stdout.read().decode()
+    print(data, end="", flush=True)
 
 proc.wait()
